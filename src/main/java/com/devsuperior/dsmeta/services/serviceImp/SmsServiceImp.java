676d5c861606a0eb01c726b4c6db8a01fd00626f
huggoparcelly <h.parcelly@gmail.com>
2023-12-05 21:46:58 -0300

    remove twilio service

--- src/main/java/com/devsuperior/dsmeta/services/serviceImp/SmsServiceImp.java
+++ src/main/java/com/devsuperior/dsmeta/services/serviceImp/SmsServiceImp.java
@@ -3,27 +3,11 @@ package com.devsuperior.dsmeta.services.serviceImp;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 import com.devsuperior.dsmeta.services.SmsService;
-import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.Message;
-import com.twilio.type.PhoneNumber;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SmsServiceImp implements SmsService {
-
-    @Value("${twilio.sid}")
-    private String twilioSid;
-
-    @Value("${twilio.key}")
-    private String twilioKey;
-
-    @Value("${twilio.phone.from}")
-    private String twilioPhoneFrom;
-
-    @Value("${twilio.phone.to}")
-    private String twilioPhoneTo;
 
     @Autowired
     private SaleRepository saleRepository;
@@ -37,13 +21,7 @@ public class SmsServiceImp implements SmsService {
         String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
                 + " com um total de R$ " + String.format("%.2f", sale.getAmount());
 
-        Twilio.init(twilioSid, twilioKey);
 
-        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
-        PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
-
-        Message message = Message.creator(to, from, msg).create();
-
-        System.out.println(message.getSid());
+        System.out.println(msg);
     }
 }
